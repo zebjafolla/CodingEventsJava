c43d9eda53b493468694a04a63a32d1fe4c894f5
John Woolbright <jwoolbright23@gmail.com>
2023-07-11 14:43:24 -0500

    forms branch

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -25,5 +25,9 @@ public class EventController {
         model.addAttribute("events", events);
         return "events/index";
     }
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
 
 }
