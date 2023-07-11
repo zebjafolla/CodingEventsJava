5c0a6a9b386ba54eb95d9f77a2aef69dc62b44ff
John Woolbright <jwoolbright23@gmail.com>
2023-07-11 15:23:29 -0500

    forms-continued branch

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,8 +2,10 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,22 +14,26 @@ import java.util.List;
  * Created by Chris Bay
  */
 @Controller
-@RequestMapping("events")
+@RequestMapping("events/")
 public class EventController {
+
+    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
+
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm() {
         return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
     }
 
 }
