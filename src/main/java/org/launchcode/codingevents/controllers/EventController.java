b6ea268bf3e208f7ab8a3f84f0785daf5dcc1a2b
John Woolbright <jwoolbright23@gmail.com>
2023-07-12 11:25:41 -0500

    fragments branch

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -13,27 +13,32 @@ import java.util.List;
 /**
  * Created by Chris Bay
  */
+/**
+ * Created by Chris Bay
+ */
 @Controller
-@RequestMapping("events/")
+@RequestMapping("events")
 public class EventController {
 
     private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     @GetMapping("create")
-    public String displayCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
+    public String processCreateEventForm(@RequestParam String eventName) {
         events.add(eventName);
-        return "redirect:";
+        return "redirect:/events";
     }
 
 }
